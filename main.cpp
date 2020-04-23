b5d74f95f39e7067caefbb8ffa7bea7a7bfbe72d
gr0nnsak <rune.langleite@gmail.com>
2020-04-23 20:35:29 +0200

    impl for opening stream defaults

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "portaudio.h"
 
+#define SAMPLE_RATE 44100
+
+static paTestData data;
+
 int main()
 {
+	PaError *err;
+
 	// Iniitalize
 	if(Pa_Initialize() == paNoError)
 	{
@@ -12,6 +18,24 @@ int main()
 	{
 		printf("PortAudio error: %s\n", Pa_GetErrorText(err));
 		return EXIT_FAILURE;
+	}
+
+	PaStream *stream;
+
+	// Open an audio I/O stream
+	err = Pa_OpenDefaultStream(
+				&stream,
+				0,			// no input channels
+				2,			// stereo output
+				paFloat32,		// 32-bit floating point output
+				SAMPLE_RATE,
+				256,			// Frames per buffer
+				patestCallback,		// Callback function
+				&data);			// Callback function pauload
+
+	if(err != paNoError)
+	{
+		printf("PortAudio error: %s\n", Pa_GetErrorText(err));
 	}
 
 	// while key is down
